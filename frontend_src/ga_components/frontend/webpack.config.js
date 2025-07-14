5dee6b143fae3266e8ae53e4a49d35e7bf4fd0db
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-07-14 11:50:30 +0545

    Add google analytics component

---
+++ frontend_src/ga_components/frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: `
+        <!DOCTYPE html>
+        <html>
+          <head><meta charset="utf-8"><title>GA Component</title></head>
+          <body><div id="root"></div></body>
+        </html>
+      `,
+      inject: "body"
+    })
+  ]
+};
